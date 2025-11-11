482f2977f4b6a756923613dabc57ce19b4ae94ae
i-am-dahee <hxxly02@gmail.com>
2025-11-11 19:24:04 +0900

    [level 1] Title: K번째수, Time: 1.12 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        
+        int[] answer = new int[commands.length];
+        
+        for (int i=0; i<commands.length; i++) {
+            
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int select = commands[i][2];
+            
+            int[] slice = Arrays.copyOfRange(array, start-1, end);
+            Arrays.sort(slice);
+            
+            answer[i] = slice[select-1];
+        }
+        
+        return answer;
+    }
+}
