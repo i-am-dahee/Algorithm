d0934a7849d9947603aae499c0adf10a552964ca
i-am-dahee <hxxly02@gmail.com>
2025-12-01 11:57:22 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.38 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int i = 0; i < score.length; i++) {
+            
+            pq.offer(score[i]);
+            
+            if (k < pq.size()) {
+                pq.poll();
+            }
+            
+            answer[i] = pq.peek();
+        }
+        
+        return answer;
+    }
+}
