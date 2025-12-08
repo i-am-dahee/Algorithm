7b151bbbacec04dd5871e053db8d10b7e3a04c3d
i-am-dahee <hxxly02@gmail.com>
2025-12-08 19:41:27 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 13912 KB -BaekjoonHub

--- 백준/Bronze/10172. 개/개.java
+++ 백준/Bronze/10172. 개/개.java
@@ -1,6 +1,5 @@
-public class Main {
+class Main {
     public static void main(String[] args) {
-        
         System.out.println("|\\_/|");
         System.out.println("|q p|   /}");
         System.out.println("( 0 )\"\"\"\\");
