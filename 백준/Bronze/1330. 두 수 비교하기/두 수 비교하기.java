b9054934085d053bbe3f2aa341100d20125606da
i-am-dahee <hxxly02@gmail.com>
2025-12-15 12:02:09 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 176 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
-        
+
         int A = sc.nextInt();
         int B = sc.nextInt();
-        
+
         if (A > B) {
             System.out.println(">");
         } else if (A < B) {
@@ -15,5 +15,7 @@ public class Main {
         } else {
             System.out.println("==");
         }
+
+        sc.close();
     }
 }
